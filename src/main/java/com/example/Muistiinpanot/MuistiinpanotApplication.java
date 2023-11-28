2f85218a7dbd334c6cb51bc68f76c2bb4ce1f939
Hermanni Bäckman <you@example.com>
2023-11-28 17:26:43 +0200

    Project creation

---
+++ src/main/java/com/example/Muistiinpanot/MuistiinpanotApplication.java
@@ -0,0 +1,13 @@
+package com.example.Muistiinpanot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuistiinpanotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuistiinpanotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Muistiinpanot/MuistiinpanotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Muistiinpanot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuistiinpanotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
