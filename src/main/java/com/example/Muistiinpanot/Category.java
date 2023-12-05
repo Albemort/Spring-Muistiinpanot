2221cb8891010c03a16afa4f87d52de16a1dfe19
Hermanni Bäckman <you@example.com>
2023-12-05 19:01:10 +0200

    Konstruktorit ja annotaatio

--- src/main/java/com/example/Muistiinpanot/Category.java
+++ src/main/java/com/example/Muistiinpanot/Category.java
@@ -1,15 +1,24 @@
 package com.example.Muistiinpanot;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
+import java.util.List;
+
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "categories")
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Category extends AbstractPersistable<Long> {
     @Column(name = "category_name", nullable = false)
     private String categoryName;
+
+    @ManyToMany(mappedBy = "categories")
+    private List<Event> events;
 }
