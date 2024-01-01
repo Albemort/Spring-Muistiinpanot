3e28b1c8c9298b5e207796937610d67d5ce85e37
Hermanni Bäckman <you@example.com>
2024-01-01 19:49:54 +0200

    Import LocalTime and new tables event_time and due_date.

--- src/main/java/com/example/Muistiinpanot/Event.java
+++ src/main/java/com/example/Muistiinpanot/Event.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +31,12 @@ public class Event extends AbstractPersistable<Long> {
 
     @Column(name = "event_date")
     private LocalDate eventDate;
+
+    @Column(name = "event_time")
+    private LocalTime eventTime;
+
+    @Column(name = "due_date")
+    private LocalDate eventDue;
 
     @JoinTable(
             name = "event_categories",
